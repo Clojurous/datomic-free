8943a29b78083749f8d1bbb258063d347a376893
Thomas Spellman <thos37@gmail.com>
2015-10-09 12:21:27 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sed "/host=0.0.0.0/a alt-host=${ALT_HOST:-127.0.0.1}" -i transactor.properties
+#sed "/host=0.0.0.0/a alt-host=${ALT_HOST:-127.0.0.1}" -i transactor.properties
 
-bin/transactor transactor.properties
+bin/transactor -Xmx=${XMX:256m} -Xms=${XMS:255m} transactor.properties
